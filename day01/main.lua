da6e33868faf4efdbc99115abc166d59fcc8d8da
Seng <engelhardt_stefan@gmx.de>
2023-07-07 21:19:44 +0200

    solved day 1 part 2

--- day01/main.lua
+++ day01/main.lua
@@ -10,6 +10,21 @@ local function solve_part1(numbers)
     )
 end
 
+local function solve_part2(numbers)
+    local frequencies = { [0] = 1 }
+    local index = 1
+    local current = 0
+    while true do
+        current = current + numbers[index]
+        index = aux.wrap(index + 1, #numbers)
+        if frequencies[current] then
+            return current
+        else
+            frequencies[current] = 1
+        end
+    end
+end
+
 local function main()
     local file = io.open("day01/input.txt", "r")
     if not file then return end
@@ -21,8 +36,10 @@ local function main()
     file:close()
 
     local part1 = solve_part1(numbers)
+    local part2 = solve_part2(numbers)
 
     print(part1)
+    print(part2)
 end
 
 main()
