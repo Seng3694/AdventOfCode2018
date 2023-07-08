38b93ac4d2b23722d7542952102d2b530f15b788
Seng <engelhardt_stefan@gmx.de>
2023-07-08 09:45:05 +0200

    solved day 2 part 2

--- day02/main.lua
+++ day02/main.lua
@@ -40,6 +40,34 @@ local function solve_part1(ids)
     return twos * threes
 end
 
+local function count_same_letters(a, b)
+    -- assumes that a and b have the same length
+    local count = 0
+    local lastDifference = 0
+    for i = 1, #a do
+        if a:sub(i, i) == b:sub(i, i) then
+            count = count + 1
+        else
+            lastDifference = i
+        end
+    end
+    return count, lastDifference
+end
+
+local function solve_part2(ids)
+    for i = 1, #ids - 1 do
+        local a = ids[i]
+        for j = i, #ids do
+            local b = ids[j]
+            local sameLetterCount, lastDifference = count_same_letters(a, b)
+            local difference = #a - sameLetterCount
+            if difference == 1 then
+                return a:sub(1, lastDifference - 1) .. a:sub(lastDifference + 1)
+            end
+        end
+    end
+end
+
 local function main()
     local file = io.open("day02/input.txt", "r")
     if not file then return end
@@ -51,8 +79,10 @@ local function main()
     file:close()
 
     local part1 = solve_part1(ids)
+    local part2 = solve_part2(ids)
 
     print(part1)
+    print(part2)
 end
 
 main()
