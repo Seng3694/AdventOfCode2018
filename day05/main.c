f1f6948e6bc25cdad3470b8e9a9e02f72e85a945
Seng <engelhardt_stefan@gmx.de>
2023-09-21 16:32:33 +0200

    improved day 5

--- day05/main.c
+++ day05/main.c
@@ -5,63 +5,45 @@
 #include <aoc/aoc.h>
 #include <aoc/mem.h>
 
-#define EMPTY_CHAR '#'
-
 static inline int8_t get_abs(const int8_t n) {
   const int8_t mask = n >> (sizeof(int8_t) * CHAR_BIT - 1);
   return (n + mask) ^ mask;
 }
 
-static uint32_t solve_part1(char *const text, const size_t textLength,
-                            uint32_t polymerLength) {
+static uint32_t solve_part1(char *const text, size_t textLength) {
   bool changed = true;
-  size_t lastNonEmptyIndex = textLength - 1;
-  while (changed) {
+  do {
     changed = false;
     for (int32_t i = (int32_t)textLength - 2; i >= 0; --i) {
-      if (text[i] == EMPTY_CHAR)
-        continue;
-
-      if (get_abs(text[lastNonEmptyIndex] - text[i]) == 32) {
-        text[i] = EMPTY_CHAR;
-        text[lastNonEmptyIndex] = EMPTY_CHAR;
-        polymerLength -= 2;
+      if (get_abs(text[i + 1] - text[i]) == 32) {
+        textLength -= 2;
         changed = true;
-
-        while (i >= 0) {
-          if (text[i] != EMPTY_CHAR) {
-            lastNonEmptyIndex = i;
-            break;
-          }
-          i--;
-        }
-
-      } else {
-        lastNonEmptyIndex = i;
+        AocMemCopy(&text[i], &text[i + 2], textLength - i);
       }
     }
+  } while (changed);
+  return textLength - 1;
+}
+
+static inline size_t custom_copy(char *const dest, const char *const src,
+                                 const size_t bytes, const char ignore) {
+  size_t length = 0;
+  for (size_t i = 0; i < bytes; ++i) {
+    if ((src[i] & ~32) != ignore) {
+      dest[length++] = src[i];
+    }
   }
-  return polymerLength;
+  return length;
 }
 
 static uint32_t solve_part2(char *const copy, const char *const text,
                             const size_t length) {
-  uint32_t minLength = UINT32_MAX;
+  uint32_t minLength = (uint32_t)length;
   for (char c = 'A'; c <= 'Z'; ++c) {
-    AocMemCopy(copy, text, length + 1);
-
-    // flag chars
-    uint32_t flaggedChars = 0;
-    for (char *current = copy; *current != '\0'; ++current) {
-      if (*current == c || *current == c + 32) {
-        *current = EMPTY_CHAR;
-        flaggedChars++;
-      }
-    }
-    const uint32_t newLength = solve_part1(copy, length, length - flaggedChars);
-    if (newLength < minLength) {
+    const size_t copyLength = custom_copy(copy, text, length, c);
+    const uint32_t newLength = solve_part1(copy, copyLength);
+    if (newLength < minLength)
       minLength = newLength;
-    }
   }
   return minLength;
 }
@@ -70,13 +52,11 @@ int main(void) {
   char *text = NULL;
   size_t length = 0;
   AocReadFileToString("day05/input.txt", &text, &length);
-  AocTrimRight(text, &length);
 
-  char *copy = malloc(length + 1);
-  AocMemCopy(copy, text, length + 1);
+  char *copy = malloc(length);
+  AocMemCopy(copy, text, length);
 
-  const uint32_t part1 = solve_part1(copy, length, length);
-
+  const uint32_t part1 = solve_part1(copy, length);
   const uint32_t part2 = solve_part2(copy, text, length);
 
   printf("%u\n", part1);
