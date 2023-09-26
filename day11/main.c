dbba78ae23601b4b92a430e1f8b3fb7fd37ef826
Seng <engelhardt_stefan@gmx.de>
2023-09-26 16:21:13 +0200

    solved day 11 part 2

--- day11/main.c
+++ day11/main.c
@@ -3,7 +3,6 @@
 
 #define SERIAL_NUMBER 5177 // input
 #define GRID_SIZE 300
-#define SQUARE_SIZE 3
 
 static void calc_fuel_values(int *const grid) {
   for (int y = 0; y < GRID_SIZE; ++y) {
@@ -19,14 +18,14 @@ static void calc_fuel_values(int *const grid) {
   }
 }
 
-static void solve_part1(const int *const grid, int *const outX,
-                        int *const outY) {
+static int solve_part1(const int *const grid, const int squareSize,
+                       int *const outX, int *const outY) {
   int largestPower = 0;
-  for (int y = 0; y < (GRID_SIZE - SQUARE_SIZE); ++y) {
-    for (int x = 0; x < (GRID_SIZE - SQUARE_SIZE); ++x) {
+  for (int y = 0; y < (GRID_SIZE - squareSize); ++y) {
+    for (int x = 0; x < (GRID_SIZE - squareSize); ++x) {
       int totalPower = 0;
-      for (int sy = 0; sy < SQUARE_SIZE; ++sy) {
-        for (int sx = 0; sx < SQUARE_SIZE; ++sx) {
+      for (int sy = 0; sy < squareSize; ++sy) {
+        for (int sx = 0; sx < squareSize; ++sx) {
           const int index = (y + sy) * GRID_SIZE + (x + sx);
           totalPower += grid[index];
         }
@@ -38,13 +37,36 @@ static void solve_part1(const int *const grid, int *const outX,
       }
     }
   }
+  return largestPower;
+}
+
+static void solve_part2(const int *const grid, int *const outX, int *const outY,
+                        int *const outSize) {
+  int largestPower = 0;
+
+  for (int size = 1; size <= 300; ++size) {
+    int x = 0;
+    int y = 0;
+    int power = solve_part1(grid, size, &x, &y);
+    if (power > largestPower) {
+      *outX = x;
+      *outY = y;
+      *outSize = size;
+      largestPower = power;
+    }
+  }
 }
 
 int main(void) {
   int grid[GRID_SIZE * GRID_SIZE] = {0};
   calc_fuel_values(grid);
-  int x = 0;
-  int y = 0;
-  solve_part1(grid, &x, &y);
-  printf("%d,%d\n", x, y);
+  int x1 = 0;
+  int y1 = 0;
+  solve_part1(grid, 3, &x1, &y1);
+  int x2 = 0;
+  int y2 = 0;
+  int size = 0;
+  solve_part2(grid, &x2, &y2, &size);
+  printf("%d,%d\n", x1, y1);
+  printf("%d,%d,%d\n", x2, y2, size);
 }
