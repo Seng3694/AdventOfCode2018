b63387c1e9d8f5c3b7937e2fc0b3a193f52aa225
Seng <engelhardt_stefan@gmx.de>
2023-10-11 04:10:47 +0200

    solved day 18 part 2

--- day18/main.c
+++ day18/main.c
@@ -1,6 +1,7 @@
 #include <aoc/aoc.h>
 #include <aoc/mem.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MAP_SIZE 50
 #define MAP_TOTAL_SIZE (MAP_SIZE * MAP_SIZE)
@@ -69,23 +70,59 @@ static void tick(map front, map back) {
   }
 }
 
-static int solve_part1(map m) {
-  map buffer = {0};
-
-  tile_type *front = m;
-  tile_type *back = buffer;
-
-  for (int i = 0; i < 10; ++i) {
-    tick(front, back);
-    tile_type *tmp = front;
-    front = back;
-    back = tmp;
-  }
-
+static int count_total_resources(const map m) {
   int counts[3] = {0};
   for (int i = 0; i < MAP_TOTAL_SIZE; ++i)
     counts[m[i]]++;
   return counts[TILE_TYPE_LUMBERYARD] * counts[TILE_TYPE_TREES];
+}
+
+static void solve(map m, int *const part1, int *const part2) {
+  map buffer = {0};
+  // use double buffering
+  tile_type *front = m;
+  tile_type *back = buffer;
+
+#define DO_TICK()                                                              \
+  do {                                                                         \
+    tick(front, back);                                                         \
+    tile_type *tmp = front;                                                    \
+    front = back;                                                              \
+    back = tmp;                                                                \
+  } while (0)
+
+  int i = 0;
+  for (i = 0; i < 10; ++i)
+    DO_TICK();
+
+  *part1 = count_total_resources(front);
+
+  // arbitrary number
+  const int extraRuns = 500;
+
+  for (; i < extraRuns; ++i)
+    DO_TICK();
+
+  // take snapshot of current state
+  map snapshot;
+  memcpy(snapshot, m, sizeof(map));
+
+  // repeat transforming until the snapshot state is reached again
+  for (;; ++i) {
+    DO_TICK();
+    if (memcmp(snapshot, front, sizeof(map)) == 0)
+      break;
+  }
+
+  // calculate difference
+  const int repeat = i - (extraRuns - 1);
+  // calclate missing runs
+  const int offset = (1000000000 - extraRuns) % repeat;
+  // do missing runs
+  for (int j = 0; j < offset; ++j)
+    DO_TICK();
+
+  *part2 = count_total_resources(front);
 }
 
 int main(void) {
@@ -95,8 +132,11 @@ int main(void) {
   AocReadFileToString("day18/input.txt", &contents, &length);
   parse(contents, m);
 
-  const int part1 = solve_part1(m);
+  int part1 = 0;
+  int part2 = 0;
+  solve(m, &part1, &part2);
   printf("%d\n", part1);
+  printf("%d\n", part2);
 
   AocFree(contents);
 }
